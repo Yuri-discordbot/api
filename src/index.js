a583c10d95f099b5dbec51a6b2aa15a208f16729
Catherine Gilbert <catherine.gilbert15@gmail.com>
2021-07-11 22:34:45 -0400

    Added cors support for yuri-configurator

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 // noinspection JSCheckFunctionSignatures
 app.use(morgan("dev"));
+
+const allowedOrigins = ["https://configurator.yuri.olaren.dev", "https://configurator.beta.yuri.olaren.dev"];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+}
+
+// noinspection JSCheckFunctionSignatures
+app.use(cors(corsOptions))
 
 app.get("/ping", async (req, res) => {
     res.send({
